94dcfd06eae062d7000cef34d83f09c90b27ff81
jofredogivan <jofredogivan@gmail.com>
2025-05-14 21:39:05 +0000

    Create modulo2.js

---
+++ pages/pages/pages/modulo2.js
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+export default function Modulo2() {
+  return (
+    <main style={{ padding: '2rem' }}>
+      <h2>Módulo 2 - Fundamentos Bíblicos</h2>
+      <p>Vídeo da aula: <a href="https://www.youtube.com/watch?v=VIDEO_ID_2" target="_blank">Assistir</a></p>
+      <p><Link href="/">← Voltar</Link></p>
+    </main>
+  );
+}
