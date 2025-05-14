c859c975f4acbe62d655137d344502cd1cf8604f
jofredogivan <jofredogivan@gmail.com>
2025-05-14 21:40:13 +0000

    Create modulo3.js

---
+++ pages/pages/pages/pages/modulo3.js
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+export default function Modulo3() {
+  return (
+    <main style={{ padding: '2rem' }}>
+      <h2>Módulo 3 - Vida Cristã e Comunhão</h2>
+      <p>Vídeo da aula: <a href="https://www.youtube.com/watch?v=VIDEO_ID_3" target="_blank">Assistir</a></p>
+      <p><Link href="/">← Voltar</Link></p>
+    </main>
+  );
+}
